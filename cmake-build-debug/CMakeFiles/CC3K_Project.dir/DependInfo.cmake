aa8af0b4f0cb1068b2ca65d9f916de40e0781fac
罗沛加 <luopeijia@JefffffffdeMacBook-Pro.local>
2018-07-29 16:23:24 -0400

    cc3k

---
+++ cmake-build-debug/CMakeFiles/CC3K_Project.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/BaseObject.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/BaseObject.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/Cell.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/Cell.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/Chamber.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/Chamber.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/Character.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/Character.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/Display.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/Display.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/Enemy.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/Enemy.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/Floor.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/Floor.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/GameObject.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/GameObject.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/Helpers.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/Helpers.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/Hero.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/Hero.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/Item.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/Item.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/Treasure.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/Treasure.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/main.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/main.cpp.o"
+  "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/potion.cpp" "/Users/luopeijia/Desktop/cc3k project/CC3K_Project/cmake-build-debug/CMakeFiles/CC3K_Project.dir/potion.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CC3K_Project.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CC3K_Project.dir/BaseObject.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/Cell.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/Chamber.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/Character.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/Display.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/Enemy.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/Floor.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/GameObject.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/Helpers.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/Hero.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/Item.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/main.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/potion.cpp.o"
+  "CMakeFiles/CC3K_Project.dir/Treasure.cpp.o"
+  "CC3K_Project.pdb"
+  "CC3K_Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CC3K_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
